d1cdde8ba5ed03498b87be1d908065c0949994fd
MR.Knorth <46872981+ChenhaoZhu@users.noreply.github.com>
2022-02-15 21:32:02 -0500

    Add files via upload

---
+++ propagators.py
@@ -0,0 +1,141 @@
+# =============================
+# Student Names:
+# Group ID:
+# Date:
+# =============================
+# CISC 352 - W22
+# propagators.py
+# desc:
+#
+
+
+#Look for #IMPLEMENT tags in this file. These tags indicate what has
+#to be implemented to complete problem solution.
+
+'''This file will contain different constraint propagators to be used within
+   bt_search.
+
+   propagator == a function with the following template
+      propagator(csp, newly_instantiated_variable=None)
+           ==> returns (True/False, [(Variable, Value), (Variable, Value) ...]
+
+      csp is a CSP object---the propagator can use this to get access
+      to the variables and constraints of the problem. The assigned variables
+      can be accessed via methods, the values assigned can also be accessed.
+
+      newly_instaniated_variable is an optional argument.
+      if newly_instantiated_variable is not None:
+          then newly_instantiated_variable is the most
+           recently assigned variable of the search.
+      else:
+          progator is called before any assignments are made
+          in which case it must decide what processing to do
+           prior to any variables being assigned. SEE BELOW
+
+       The propagator returns True/False and a list of (Variable, Value) pairs.
+       Return is False if a deadend has been detected by the propagator.
+       in this case bt_search will backtrack
+       return is true if we can continue.
+
+      The list of variable values pairs are all of the values
+      the propagator pruned (using the variable's prune_value method).
+      bt_search NEEDS to know this in order to correctly restore these
+      values when it undoes a variable assignment.
+
+      NOTE propagator SHOULD NOT prune a value that has already been
+      pruned! Nor should it prune a value twice
+
+      PROPAGATOR called with newly_instantiated_variable = None
+      PROCESSING REQUIRED:
+        for plain backtracking (where we only check fully instantiated
+        constraints)
+        we do nothing...return true, []
+
+        for forward checking (where we only check constraints with one
+        remaining variable)
+        we look for unary constraints of the csp (constraints whose scope
+        contains only one variable) and we forward_check these constraints.
+
+        for gac we establish initial GAC by initializing the GAC queue
+        with all constaints of the csp
+
+
+      PROPAGATOR called with newly_instantiated_variable = a variable V
+      PROCESSING REQUIRED:
+         for plain backtracking we check all constraints with V (see csp method
+         get_cons_with_var) that are fully assigned.
+
+         for forward checking we forward check all constraints with V
+         that have one unassigned variable left
+
+         for gac we initialize the GAC queue with all constraints containing V.
+   '''
+
+def prop_BT(csp, newVar=None):
+    '''Do plain backtracking propagation. That is, do no
+    propagation at all. Just check fully instantiated constraints'''
+
+    if not newVar:
+        return True, []
+    for c in csp.get_cons_with_var(newVar):
+        if c.get_n_unasgn() == 0:
+            vals = []
+            vars = c.get_scope()
+            for var in vars:
+                vals.append(var.get_assigned_value())
+            if not c.check(vals):
+                return False, []
+    return True, []
+
+def prop_FC(csp, newVar=None):
+    '''Do forward checking. That is check constraints with
+       only one uninstantiated variable. Remember to keep
+       track of all pruned variable,value pairs and return '''
+    #IMPLEMENT
+    if not newVar:
+        cons = csp.get_all_cons()
+    else:
+        cons = csp.get_cons_with_var(newVar)
+    vals = []
+    for c in cons:
+        if c.get_n_unasgn() == 1 : # only one uninstantiated variable
+            c0 = c.get_unasgn_vars()[0]
+            for d_element in c0.cur_domain():
+                if c.has_support(c0, d_element) == False:
+                    path = (c0, d_element)
+                    if path not in vals:
+                        c0.prune_value(d_element) # delete it.
+                        vals.append(path)
+            if c0.cur_domain_size() == 0: # Meaning we prune every domain from c0, reaching deadend.
+                return False, vals
+    return True, vals
+
+
+def prop_GAC(csp, newVar=None):
+    '''Do GAC propagation. If newVar is None we do initial GAC enforce
+       processing all constraints. Otherwise we do GAC enforce with
+       constraints containing newVar on GAC Queue'''
+    #IMPLEMENT
+    vals = []
+    if not newVar:
+        queue = csp.get_all_cons() # local variable according to lecture
+    else:
+        queue = csp.get_cons_with_var(newVar)
+    while len(queue) != 0: # not empty
+        c0 = queue.pop(0)
+        variables_include = c0.get_scope()
+        for v in variables_include: # Removed-inconsistent-Values(Xi, X)
+            domainV = v.cur_domain()
+            for d in domainV:
+                if c0.has_support(v, d) == False: # No Y in domain(X) allows (x,Y) satisfy constraint
+                    path = (v, d)
+                    if path not in vals:
+                        v.prune_value(d) # Delete it.
+                        vals.append(path)
+                    if v.cur_domain_size() == 0: # Meaning we prune every domain from v, reaching deadend.
+                        return False, vals
+                    else:
+                        for neighbors in csp.get_cons_with_var(v):
+                            if neighbors not in queue:
+                                queue.append(neighbors) # Add (Xk, *) to queue
+    return True, vals
